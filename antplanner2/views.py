de92841437fe0f2a1b2c64a7f81a419d90f2e41e
Ryan <ryanlives@gmail.com>
2012-10-22 23:33:30 -0700

    add caching via memcache

--- antplanner2/views.py
+++ antplanner2/views.py
@@ -1,11 +1,9 @@
 from antplanner2 import app
-from flask import flash, render_template, request
-from google.appengine.api import urlfetch
-from bs4 import BeautifulSoup
+from antplanner2 import models, websoc
+from flask import flash, render_template, request, jsonify
+from google.appengine.api import memcache
 import logging
-import urllib
 
-logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 @app.route('/')
@@ -14,22 +12,35 @@ def index():
 
 @app.route('/websoc/search', methods=['GET'])
 def websoc_search_form():
-    html = urlfetch.fetch("http://websoc.reg.uci.edu").content
-    form_html = BeautifulSoup(html).find('form', action='http://websoc.reg.uci.edu/perl/WebSoc')
+    form_html = memcache.get('search')
+    if not form_html:
+        form_html = unicode(websoc.get_search().encode(formatter='html'))
+        memcache.add('search', form_html, 60 * 60 * 24)
+
     return render_template('websoc/search.html', form=form_html)
 
 @app.route('/websoc/search', methods=['POST'])
 def websoc_search():
-    encoded = urllib.urlencode(request.form)
-
-    html = urlfetch.fetch("http://websoc.reg.uci.edu",
-        payload=encoded,
-        method=urlfetch.POST,
-        headers={'Content-Type': 'application/x-www-form-urlencoded'}).content
-
-    listing_html = BeautifulSoup(html).find('div', 'course-list')
+    key = str(request.form)
+    listing_html = memcache.get(key)
+    if not listing_html:
+        listing_html = unicode(websoc.get_listing(request.form).encode(formatter='html'))
+        memcache.add(key, listing_html, 60 * 60 * 24)
     return render_template('websoc/listing.html', listing=listing_html)
 
 @app.route('/test')
 def qunit():
     return render_template('test.html')
+
+# @app.route('/schedules/add', methods=['POST'])
+# def add_schedule():
+#     username = request.form.get('username')
+#     event_content = request.form.get('events')
+#     schedule = models.Schedule(username=username, content=event_content)
+#     schedule.put()
+#     return jsonify(id=schedule.key.id())
+
+# @app.route('/schedules/<int:id_>')
+# def show_schedule(id_):
+#     content = models.Schedule.get_by_id(id_).content
+#     return render_template('index.html', x=content)
