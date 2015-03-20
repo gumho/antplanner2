8dbf16233ebb7dc8edec3de709c97ceee1c34975
Ryan Hsu <rhsu@vmware.com>
2015-03-19 22:50:31 -0700

    Make memcache usage configurable via environment variable

--- antplanner2/views.py
+++ antplanner2/views.py
@@ -3,18 +3,19 @@ from flask import render_template, request, jsonify
 from google.appengine.api import memcache
 from google.appengine.ext import db
 import logging
-import os
+from os import environ as env
 
 logger = logging.getLogger(__name__)
-dev_mode = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
-
+dev_mode = env.get('SERVER_SOFTWARE', '').startswith('Development')
+use_memcache = env['USE_MEMCACHE'].lower() == 'true'
 
 @app.route('/')
 def index():
     index_html = memcache.get('index')
     if not index_html:
         index_html = render_template('index.html')
-        memcache.add('index', index_html, 60 * 60 * 24)
+        if use_memcache:
+            memcache.add('index', index_html, 60 * 60 * 24)
     return index_html
 
 
@@ -23,7 +24,8 @@ def websoc_search_form():
     form_html = memcache.get('search')
     if not form_html:
         form_html = websoc.get_search()
-        memcache.add('search', form_html, 60 * 60 * 24)
+        if use_memcache:
+            memcache.add('search', form_html, 60 * 60 * 24)
 
     return render_template('websoc/search.html', form_content=form_html)
 
@@ -34,7 +36,8 @@ def websoc_search():
     listing_html = memcache.get(key)
     if not listing_html:
         listing_html = websoc.get_listing(request.form)
-        memcache.add(key, listing_html, 60 * 60 * 24)
+        if use_memcache:
+            memcache.add(key, listing_html, 60 * 60 * 24)
     return render_template('websoc/listing.html', listing=listing_html)
 
 
